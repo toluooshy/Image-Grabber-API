b7d55de30801bf14d2859709336a49e78e08948b
toluooshy <toluooshy@gmail.com>
2022-06-30 15:45:11 -0400

    Refactored code

--- main.py
+++ main.py
@@ -1,54 +1,7 @@
 from typing import Optional
-
-from threading import current_thread
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from bs4 import BeautifulSoup
-import requests
-
-
-class ImageScraper:
-    def __init__(self, url):
-        self.url = url
-        self.html = ""
-        self.images = []
-
-    def get_html(self):
-        """
-        This function gets the raw html code from the website defined in self.url
-
-        Takes: self
-        Use: defines self.html
-        Returns: self.html
-        """
-        try:
-            self.html = BeautifulSoup(requests.get(self.url).content, 'lxml')
-            return self.html
-        except TypeError:
-            print(
-                "This function needs an object of the type 'string' to be passed into it to work.")
-
-    def get_images(self):
-        """
-        This function gets the image src urls from the raw html defined in self.html
-
-        Takes: self
-        Use: defines self.images
-        Returns: self.images
-        """
-        imgs = self.html.find_all('img')
-        for img in imgs:
-            try:
-                if len(img['src']) > 0:
-                    if img['src'][0:2] == '//':
-                        self.images.append(img['src'][2:])
-                    elif img['src'][0:1] == '/':
-                        self.images.append(self.url+img['src'][1:])
-                    else:
-                        self.images.append(img['src'])
-            except KeyError:
-                pass
-        return self.images
+from algorithm import ImageScraper
 
 
 app = FastAPI()
@@ -72,11 +25,11 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Root Request": 200}
 
 
-@app.get("/url/{link}")
-def read_item(link: Optional[str] = None):
+@app.get("/grab/{link}")
+def grab_images(link: Optional[str] = None):
     urlimage = ImageScraper('http://' + link)
     urlimage.get_html()
     return urlimage.get_images()
